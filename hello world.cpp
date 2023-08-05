aa1c77c316d6711afb1133b3c096341df6ddb0ab
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-08-05 12:31:40 +0530

    Add files via upload

---
+++ hello world.cpp
@@ -0,0 +1,7 @@
+//hello world program
+#include<iostream>  //header file - I/O streams 
+int main() // serves as the entry point of the program ,execution of the program starts from this function.
+{
+  std::cout << "hello world!"; //std::cout represents the standard output stream
+  return 0; // returns an integer value of 0 from the main function
+}
