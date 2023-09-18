1ea4831e08e90d30d4b83724eaa63ba9e031960c
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-09-18 20:22:14 +0530

    Update check no is divide by another number.java

--- check no is divide by another number.java
+++ check no is divide by another number.java
@@ -26,3 +26,30 @@ public class Division
         }
     }
 }
+//-------------------------------------------------------------------------------------------------------------------------------
+// same code in c++  ----------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main() {
+    //  variables 
+    int num1, num2
+     // user to enter the number
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+ if (num2 != 0)
+ {
+        if (num1 % num2 == 0) 
+        {
+            cout << num1 << " is divisible by " << num2 << endl; //endl for next line
+        } else {
+            cout << num1 << " is not divisible by " << num2 << endl;
+        }
+    } else {
+        cout << "Cannot divide by zero, enter a non-zero second number." << endl;
+    }
+
+    return 0;
+}
