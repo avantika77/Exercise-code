dfa4237c32c3db1eb4451ff22ae466c9a57cdd6c
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-09-19 18:21:33 +0530

    Add files via upload

---
+++ 1 3  6 10 15 ...n terms.py
@@ -0,0 +1,17 @@
+# number of terms in the sequence
+n = int(input("Enter the number of terms: "))
+
+# Initialize the first term of the series
+first_term = 1
+
+# Initialize a list to store the sequence
+sequence = []#store numbers in the series (sequence)
+#empty list 
+
+# Generate the sequence
+for i in range(n):
+    sequence.append(first_term)
+    first_term += (i + 2)  # Add consecutive positive integers
+
+# Print the sequence
+print(sequence)
