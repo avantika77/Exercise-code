4d5d3cc69ac6078f7ec01c4c5a2145a905346538
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-09-20 18:54:54 +0530

    Add files via upload

---
+++ 1 1 1 pattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NumberPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        // Generate the pattern
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("1 ");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+    }
+}
+
+//OUTPUT//---------------------------------------------------------------------------------------------------
