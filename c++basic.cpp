9f74eb82ec0f377e84933e0629dfd73d7a451271
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-09-25 22:31:17 +0530

    Update c++basic.cpp

--- c++basic.cpp
+++ c++basic.cpp
@@ -5,20 +5,15 @@ int main()
     // this is a comment
     std::cout << "Hello World." << '\n';
     std::cout << "C++ rocks!";
+
+   char mychar{'a'};
+   int  myint{123};
+   double   mydouble{456.78};
+   std::cout << "The value of a char variable is: " << mychar << '\n';
+   std::cout << "The value of an int variable is: " << myint << '\n';
+   std::cout << "The value of a double variable is: " << mydouble << '\n';
+
+    
 }
-// int main()
-// {
-//     char mychar;
-//     int myint;
-//     double mydouble;
-// }
-// #include <iostream>
-// int main()
-// {
-//     char mychar{'a'};
-//     int myint{123};
-//     double mydouble{456.78};
-//     std::cout << "The value of a char variable is: " << mychar << '\n';
-//     std::cout << "The value of an int variable is: " << myint << '\n';
-//    std::cout << "The value of a double variable is: " << mydouble << '\n';
-// }
+
+
