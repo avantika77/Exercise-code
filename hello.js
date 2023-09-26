ad7a6805e64ac0bb54c56ac50e50885dbae15779
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-09-26 21:18:44 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,5 @@
+console.log("hello artificial world!"); // hello world
+let a=67;    // let is used for variable var is older version vasr is keyword
+console.log(a);
+var b ="goodbye"; // var can be used for variable
+console.log(b);
