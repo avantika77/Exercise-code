651076a154d272abc29c9a6aafd0a09eba404320
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-09-26 21:46:55 +0530

    Update c++basic.cpp

--- c++basic.cpp
+++ c++basic.cpp
@@ -1,5 +1,5 @@
 // c++ basic
-#include <iostream> //header file
+#include <iostream> //header file-------------------------------------------------------------------------------------
 int main()
 {
     // this is a comment
@@ -11,9 +11,16 @@ int main()
    double   mydouble{456.78};
    std::cout << "The value of a char variable is: " << mychar << '\n';
    std::cout << "The value of an int variable is: " << myint << '\n';
-   std::cout << "The value of a double variable is: " << mydouble << '\n';
-
-    
+   std::cout << "The value of a double variable is: " << mydouble << '\n';    
 }
-
+//The assignment operator = assigns a value to a variable / object: -----------------------------------------------------
+int main()
+{
+    char mychar = 'c';    // define a char variable mychar
+    mychar = 'd';         // assign a new value to mychar
+    int x = 123;          // define an integer variable x
+    x = 456;              // assign a new value to x
+    int y = 789;          // define a new integer variable y
+    y = x;                // assing a value of x to it
+}
 
