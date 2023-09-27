3f74396e3071c9b6e433216c4006c52b3fa6a437
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-09-27 22:07:54 +0530

    Update c++basic.cpp

--- c++basic.cpp
+++ c++basic.cpp
@@ -1,5 +1,5 @@
 // c++ basic
-#include <iostream> //header file-------------------------------------------------------------------------------------
+#include <iostream> //header file--------------------------------------------------------------------------------------------
 int main()
 {
     // this is a comment
@@ -13,7 +13,7 @@ int main()
    std::cout << "The value of an int variable is: " << myint << '\n';
    std::cout << "The value of a double variable is: " << mydouble << '\n';    
 }
-//The assignment operator = assigns a value to a variable / object: -----------------------------------------------------
+//The assignment operator = assigns a value to a variable / object: ----------------------------------------------------------
 int main()
 {
     char mychar = 'c';    // define a char variable mychar
@@ -23,11 +23,22 @@ int main()
     int y = 789;          // define a new integer variable y
     y = x;                // assing a value of x to it
 }
-#Arithmetic Operators -----------------------------------------------------------------------------------------
+#Arithmetic Operators -----------------------------------------------------------------------------------------------------
 #We can do arithmetic operations using arithmetic operators. Some of them are:
 + // addition
 - // subtraction
 * // multiplication
 / // division
 % // modulo
-    -----------------------------------------------------------------------------------------
+    ------------------------------------------------------------------------------------------------------------------------
+#include <iostream> //header file-------------------------------------------------------------------------------------------
+int main()
+{
+    int x = 123;
+    int y = 456;
+    int z = x + y; // add
+    z = x - y; // subtract
+    z = x * y; // multi.
+    z = x / y; // division
+    std::cout << "The value of z is: " << z << '\n';
+}
