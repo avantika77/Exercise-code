bf570bcbbc03748891e859c096968a9eb99a45b4
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-09-29 12:07:35 +0530

    Update c++basic.cpp

--- c++basic.cpp
+++ c++basic.cpp
@@ -70,4 +70,13 @@ operators allow us to perform an arithmetic operation and assign a result with o
 *= // compound multiplication
 /= // compound division
 %= // compound modulo
-
+#include <iostream>
+int main()
+{
+    int x = 123;
+    x += 10;    // the same as x = x + 10
+    x -= 10;    // the same as x = x - 10
+    x *= 2;     // the same as x = x * 2
+    x /= 3;     // the same as x = x / 3
+    std::cout << "The value of x is: " << x;
+}
