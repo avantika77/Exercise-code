32c6f0d3dfb1f03990cb859baaaf4a4e738e7e58
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-09-29 12:36:41 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,16 @@
+let a=null;
+let b=567
+let c= false
+//Let d= BigInt("234")+("12")
+let e="javascript";
+let f=Symbol("this is symbol")
+console.log(a,b,c,e,f)
+
+//object in javascript are non primitive data types in javascript
+const name={
+    "number1": false,
+    "number2": 23,
+    "number3": 5,
+    "number4": true
+}
+console.log(name["number4"])
