2ebc5299ac9ce83d2e54734c67f70d6c456890c9
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-10-01 23:33:14 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,10 @@
+//create a variable of type string and add a number to it
+let a= "hello"
+let b=88
+console.log(a+b)
+console.log(typeof(a+b))
+const v={
+    name: "hello",  
+}
+v['game']="football"
+console.log(v)
