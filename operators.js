dcea68fa166c2a93afec55c46d5193275e421d6c
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-10-11 19:00:58 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,32 @@
+//add number to  a a string
+let a=12
+let b ="avi"
+console.log("a+b is ",(a+b))
+/*const a1={
+    name: "Avi"
+   // section: 2
+    //marks: true
+}
+a1[name]="avi"
+console.log(a1);
+*/
+
+//OPERATORS
+let t=23;
+let v=7;
+console.log("t+v is ",(t+v));   
+console.log("t**v is ",(t**v));  
+console.log("t%v is ",(t%v));  
+console.log("t++ is ",(t++));
+console.log("++t is ",(++t));   
+console.log("++v is ",(++v));   
+console.log("v++ is ",(v++));   
+console.log("v= is ",(v));   
+
+let k=9
+let m=4
+console.log (k<4 &&k==9)
+console.log (k>m ||k==8)
+console.log(!false)
+console.log(!true)
+ 
