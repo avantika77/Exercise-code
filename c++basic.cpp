369052aa1d0ffff925d1211aa162aa4d2bf79121
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-10-14 19:23:09 +0530

    Update c++basic.cpp

--- c++basic.cpp
+++ c++basic.cpp
@@ -235,3 +235,14 @@ int main()
     int arr[5] = { 10, 20, 30, 40, 50 };
     arr[0] = 100; // change the value of the first array element
 }
+int main()
+{
+    int arr[5] = { 10, 20, 30, 40, 50 };
+    arr[4] = 500; // change the value of the last array element
+}
+int main()
+{
+    int arr[5] = { 10, 20, 30, 40, 50 };
+    arr[4] = 500; // change the value of the last array element
+}
+[In modern C++, we should prefer the std::array and std::vector containers to raw arrays.]
