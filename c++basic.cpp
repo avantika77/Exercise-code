dfbeca1159f79940dbe056e3e2e0e3ce8ee5e483
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-10-16 18:49:22 +0530

    Update c++basic.cpp

--- c++basic.cpp
+++ c++basic.cpp
@@ -259,3 +259,14 @@ int main()
     *p = 456; // change the value of pointed-to object
     std::cout << "The value of x is: " << x;
 }
+References
+A reference type is an alias to an existing object in memory. References must be initialized.
+ int main()
+{
+    int x = 123;
+    int& y = x;
+    x = 456;
+    // both x and y now hold the value of 456
+    y = 789;
+    // both x and y now hold the value of 789
+}   
