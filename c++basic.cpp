e5c8432a86f5debd7bf9dcd918204bf27ac11b85
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-10-19 20:02:12 +0530

    Update c++basic.cpp

--- c++basic.cpp
+++ c++basic.cpp
@@ -304,3 +304,13 @@ int main()
     char c = '!';
     s += c;
 }
+#include <iostream>
+#include <string>
+int main()
+{
+    std::string s1 = "Hello ";
+    std::string s2 = "World.";
+    std::string s3 = s1 + s2;
+    std::cout << s3;
+}
+Type string is the so-called class-template 
