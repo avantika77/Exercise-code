eda766f0246c4a443abe4a71772f867a9ffbf05e
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-10-20 19:48:41 +0530

    Update c++basic.cpp

--- c++basic.cpp
+++ c++basic.cpp
@@ -314,3 +314,17 @@ int main()
     std::cout << s3;
 }
 Type string is the so-called class-template 
+Accessing Characters
+Individual characters of a string can be accessed through a subscript operator [] or via a member function .at(index). The index starts at 0. Example:
+#include <iostream>
+#include <string>
+int main()
+{
+    std::string s = "Hello World.";
+    char c1 = s[0];        // 'H'
+    char c2 = s.at(0);     // 'H';
+    char c3 = s[6];        // 'W'
+    char c4 = s.at(6);     // 'W';
+    std::cout << "First character: " << c1 << ", sixth character: " << c3;
+}
+Individual characters of a string can be 
