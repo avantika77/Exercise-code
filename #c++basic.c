ac4700a2c0fd08bb776dc29269ce1bc74c42c70c
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-11-04 16:31:08 +0530

    Update #c++basic.c

--- #c++basic.c
+++ #c++basic.c
@@ -1,2 +1,9 @@
-//15 chapter 
-//
+//Write a program that defines and initializes an array of five doubles. Change and print the values of the first and last array elements.
+#include <iostream>
+int main()
+{
+    double arr[5] = { 1.23, 2.45, 8.52, 6.3, 10.15 };
+    arr[0] = 2.56;
+    arr[4] = 3.14;
+    std::cout << "The first array element is: " << arr[0] << '\n';
+    std::cout << "The last array element is: " << arr[4] << '\n';
