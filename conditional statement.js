6d3cba0027e17ca0715f9a9bae6c2425554c721d
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-11-06 21:59:37 +0530

    Add files via upload

---
+++ conditional statement.js
@@ -0,0 +1,18 @@
+/*
+conditional statement
+if satemeent
+if..else..statement
+if..else..if ..else..statement
+*/
+let a =prompt ("enter ypur age=");
+console.log (typeof a);
+a=Number.parseInt(a);
+console.log(typeof a);
+if(a<10)
+{
+alert("This is invalid age ");
+}
+else(a>18)
+{
+alert("This can be valid age")
+}
