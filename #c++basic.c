eee3703d5f2bdbc0a7d982b08b48b2407edc07c3
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-11-07 20:52:07 +0530

    Update #c++basic.c

--- #c++basic.c
+++ #c++basic.c
@@ -23,3 +23,8 @@ int main()
 {
     double mydouble = 3.14;
     double& myreference = mydouble;
+myreference = 6.28;
+    std::cout << "The values are: " << mydouble << " and " << myreference << '\n';
+    mydouble = 9.45;
+    std::cout << "The values are: " << mydouble << " and " << myreference << '\n';
+}
