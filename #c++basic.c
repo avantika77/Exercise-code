c491ed6e309b58c8f3d199a9b28bb8676a53559f
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-11-14 20:39:10 +0530

    Update #c++basic.c

--- #c++basic.c
+++ #c++basic.c
@@ -81,3 +81,10 @@ std::string s = "Hello C++ World.";
     auto mysubstringfound = s.find(mysubstring);
     if (mysubstringfound != std::string::npos)
     {
+std::cout << "Substring found at position: " << mysubstringfound << '\n';
+    }
+    else
+    {
+        std::cout << "Substring was not found." << '\n';
+    }
+}
