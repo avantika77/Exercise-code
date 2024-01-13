8e245be66d17992d4832f3e5e332064a1e4e054a
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2024-01-13 23:42:11 +0530

    Update hello world.cpp

--- hello world.cpp
+++ hello world.cpp
@@ -5,3 +5,4 @@ int main() // serves as the entry point of the program ,execution of the program
   std::cout << "hello world!"; //std::cout represents the standard output stream
   return 0; // returns an integer value of 0 from the main function
 }
+______________________________________
