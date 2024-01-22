1651e223b4861c9deb32d0eba7d74174a2242bb8
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2024-01-22 18:07:08 +0530

    Update @Fibonacci number.java

--- @Fibonacci number.java
+++ @Fibonacci number.java
@@ -1,31 +1,34 @@
-// #include <iostream>
-// //  Fibonacci number
-// int fibonacci(int n)
-// {
-//     if (n <= 1)
-//     {
-//         return n;
-//     }
-//     else
-//     {
-//         return fibonacci(n - 1) + fibonacci(n - 2);
-//     }
-// }
+//Fibonacci Numbers-The Fibonacci sequence is an infinite series of numbers in which each number (Fibonacci number) is the sum of the two preceding ones, usually starting with 0 and 1. 
+//Mathematically, it is defined by the recurrence relation:
+//F(n)=F(n−1)+F(n−2)
+#include <iostream>
+//  Fibonacci number
+int fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
 
-// int main()
-// {
+int main()
+{
 
-//     int num;
-//     std::cout << "Enter the number of Fibonacci numbers to generate: ";
-//     std::cin >> num;
+    int num;
+    std::cout << "Enter the number of Fibonacci numbers to generate: ";
+    std::cin >> num;
 
-//     for (int i = 0; i < num; i++)
-//     {
-//         std::cout << fibonacci(i) << " ";
-//     }
+    for (int i = 0; i < num; i++)
+    {
+        std::cout << fibonacci(i) << " ";
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 //========================================================================================================
 //Fibonacci number function
 def fibonacci(n):
