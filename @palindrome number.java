d340370d3ab9d2db1960f5ecd5d80ec01d3e0dd1
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2024-02-04 21:00:32 +0530

    Update @palindrome number.java

--- @palindrome number.java
+++ @palindrome number.java
@@ -1,18 +1,32 @@
-    class PalindromeExample{  
-     public static void main(String args[]){  
-      int r,sum=0,temp;    
-      int n=454;//It is the number variable to be checked for palindrome  
-      
-      temp=n;    
-      while(n>0){    
-       r=n%10;  //getting remainder  
-       sum=(sum*10)+r;    
-       n=n/10;    
-      }    
-      if(temp==sum)    
-       System.out.println("palindrome number ");    
-      else    
-       System.out.println("not palindrome");    
-    }  
-    }  
-___________________________________________________
+ import java.util.Scanner; //Imports the Scanner class from the java.util package, which is used for taking user input
+
+class PalindromeExample //class named {
+    public static void main(String args[]) { //Entry point of the program.
+        String original, reverse = "";  // Declares two string variables, original to store the user input and reverse to store the reversed string.
+
+        // Taking user input
+        Scanner scanner = new Scanner(System.in); //Creates a Scanner object to read user input.
+        System.out.println("Enter a string to check for palindrome:");
+        original = scanner.nextLine(); //Takes a string input from the user
+
+        int length = original.length();
+
+        // Reversing the string
+        for (int i = length - 1; i >= 0; i--) {
+            reverse = reverse + original.charAt(i); //Reverses the string by iterating through its characters from the end to the beginning.
+        }
+
+        // Checking if the original and reversed strings are equal
+        if (original.equals(reverse))
+            System.out.println("Palindrome string");
+        else
+            System.out.println("Not palindrome");
+
+        // Closing the scanner to avoid resource leaks
+        scanner.close(); //Closes the Scanner to release resources
+    }
+}
+=================================================================================================================================================
+Enter a number to check for palindrome:
+787
+Palindrome number
