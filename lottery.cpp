cf29133702c215a3ff44ebf7f968d2b06574cb17
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2024-04-08 22:09:42 +0530

    Add files via upload

---
+++ lottery.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+// Base class
+class Lotto {
+private:
+    int nums[6];
+
+public:
+    // Constructor
+    Lotto() {
+        srand(time(NULL));
+        for (int i = 0; i < 5; i++) {
+            nums[i] = (rand() % 69) + 1; // Random number between 1 and 69
+        }
+        nums[5] = (rand() % 26) + 1; // Random number between 1 and 26
+    }
+
+    // Member function to display lottery numbers
+    void getLotto() {
+        cout << "Your lottery numbers are: " << endl;
+        for (int i = 0; i < 6; i++) {
+            cout << nums[i] << " ";
+        }
+        cout << endl;
+        cout << "Good Luck!" << endl;
+    }
+};
+
+int main() {
+    Lotto myLotto;
+    myLotto.getLotto();
+    return 0;
+}
